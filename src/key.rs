570e4ad8e2ce08e105ec4f364162f7a05a5d6d14
Amit Prasad <mail@amitprasad.dev>
2024-10-19 14:04:26 -0500

    Extract Key

---
+++ src/key.rs
@@ -0,0 +1,39 @@
+pub trait Key {
+    type Error;
+
+    fn encode(&self) -> Vec<u8>;
+
+    /// Decode a key from a byte slice, consuming the bytes necessary to decode the key.
+    fn decode(bytes: &mut &[u8]) -> Result<Self, Self::Error>
+    where
+        Self: Sized;
+}
+
+#[derive(Debug)]
+pub struct CompoundKey<T: Key, U: Key>(pub T, pub U);
+impl<K: Key> CompoundKey<K, ()> {
+    pub fn new(key: K) -> Self {
+        CompoundKey(key, ())
+    }
+}
+
+impl<T: Key, U: Key> Key for CompoundKey<T, U> {
+    type Error = (Option<T::Error>, Option<U::Error>);
+    fn encode(&self) -> Vec<u8> {
+        let mut bytes = self.0.encode();
+        bytes.extend_from_slice(&self.1.encode());
+        bytes
+    }
+
+    fn decode(bytes: &mut &[u8]) -> Result<Self, Self::Error> {
+        let key = T::decode(bytes).map_err(|e| (Some(e), None))?;
+        let unit = U::decode(bytes).map_err(|e| (None, Some(e)))?;
+        Ok(CompoundKey(key, unit))
+    }
+}
+
+impl<T: Key, U: Key> CompoundKey<T, U> {
+    pub fn push<NewK: Key>(self, key: NewK) -> CompoundKey<T, CompoundKey<U, NewK>> {
+        CompoundKey(self.0, CompoundKey(self.1, key))
+    }
+}
--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,6 @@
 #![allow(unused)]
 
+mod key;
 mod map;
 mod sealed;
 mod serialization;
@@ -9,47 +10,8 @@ use std::{fmt::Debug, marker::PhantomData};
 
 use disjoint_impls::disjoint_impls;
 
+use key::{CompoundKey, Key};
 use sealed::*;
-
-pub trait Key {
-    type Error;
-
-    fn encode(&self) -> Vec<u8>;
-
-    /// Decode a key from a byte slice, consuming the bytes necessary to decode the key.
-    fn decode(bytes: &mut &[u8]) -> Result<Self, Self::Error>
-    where
-        Self: Sized;
-}
-
-#[derive(Debug)]
-pub struct CompoundKey<T: Key, U: Key>(T, U);
-impl<K: Key> CompoundKey<K, ()> {
-    pub fn new(key: K) -> Self {
-        CompoundKey(key, ())
-    }
-}
-
-impl<T: Key, U: Key> Key for CompoundKey<T, U> {
-    type Error = (Option<T::Error>, Option<U::Error>);
-    fn encode(&self) -> Vec<u8> {
-        let mut bytes = self.0.encode();
-        bytes.extend_from_slice(&self.1.encode());
-        bytes
-    }
-
-    fn decode(bytes: &mut &[u8]) -> Result<Self, Self::Error> {
-        let key = T::decode(bytes).map_err(|e| (Some(e), None))?;
-        let unit = U::decode(bytes).map_err(|e| (None, Some(e)))?;
-        Ok(CompoundKey(key, unit))
-    }
-}
-
-impl<T: Key, U: Key> CompoundKey<T, U> {
-    pub fn push<NewK: Key>(self, key: NewK) -> CompoundKey<T, CompoundKey<U, NewK>> {
-        CompoundKey(self.0, CompoundKey(self.1, key))
-    }
-}
 
 pub trait NodeType: Sealed {}
 
