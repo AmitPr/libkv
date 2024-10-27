6b0ee65ea3b2e60cafb11932515bfab9ef3c0aae
Amit Prasad <mail@amitprasad.dev>
2024-10-27 18:39:39 -0500

    folders

--- src/lib.rs
+++ src/lib.rs
@@ -1,25 +1,16 @@
 mod container;
 mod error;
-mod item;
 mod key_serialization;
-mod map;
-mod queue;
 mod serialization;
 mod storage;
-mod vector;
+mod structures;
 
 pub use container::{Container, DataStructure, DsIter, NonTerminal, Terminal};
 pub use error::{KeyDeserializeError, KeySerializeError, StorageError};
 pub use key_serialization::{KeySerde, KeyType};
 pub use serialization::{Codec, Decodable, Encodable, Encoding};
 pub use storage::{Iter, IterableStorage, Order, Storage, StorageMut};
-
-pub use item::Item;
-pub use map::Map;
-pub use queue::PriorityQueue;
-
-#[allow(unused_imports)]
-pub(crate) use vector::Vector;
+pub use structures::*;
 
 #[cfg(test)]
 pub mod mock;
---
+++ src/structures/mod.rs
@@ -0,0 +1,6 @@
+mod item;
+mod map;
+mod queue;
+mod vector;
+
+pub use {item::Item, map::Map, queue::PriorityQueue /*vector::Vector*/};
