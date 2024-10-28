5bf9eb0fb8aeff444f71ae1824c73b3a7e2a8695
Amit Prasad <mail@amitprasad.dev>
2024-10-27 21:55:55 -0500

    BorshEncoding

--- src/serialization.rs
+++ src/serialization.rs
@@ -39,3 +39,55 @@ impl<T: KeySerde> Decodable<KeyEncoding> for T {
         T::decode(&mut bytes)
     }
 }
+
+// /// Encoding that allows any type implementing `serde::Serialize` and `serde::Deserialize` to be
+// /// used as a codec.
+// #[cfg(feature = "serde")]
+// #[derive(Debug, Clone, Copy, PartialEq, Eq)]
+// pub struct SerdeEncoding<'de, S: serde::Serializer, D: serde::Deserializer<'de>>(
+//     std::marker::PhantomData<(S, &'de D)>,
+// );
+
+// #[cfg(feature = "serde")]
+// impl<'de, S: serde::Serializer, D: serde::Deserializer<'de>> Encoding for SerdeEncoding<'de, S, D> {
+//     type EncodeError = S::Error;
+//     type DecodeError = D::Error;
+// }
+
+// #[cfg(feature = "serde")]
+// impl<'de, T, S: serde::Serializer, D: serde::Deserializer<'de>> Encodable<SerdeEncoding<'de, S, D>>
+//     for T
+// where
+//     T: serde::Serialize,
+// {
+//     fn encode(&self) -> Result<Vec<u8>, S::Error> {
+//         // serde_json::to_string uses 128 bytes as the default capacity
+//         let mut buf = Vec::with_capacity(128);
+//         S::serialize_
+//         S::serialize
+//     }
+// }
+
+#[cfg(feature = "borsh")]
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+pub struct BorshEncoding;
+
+#[cfg(feature = "borsh")]
+impl Encoding for BorshEncoding {
+    type EncodeError = borsh::io::Error;
+    type DecodeError = borsh::io::Error;
+}
+
+#[cfg(feature = "borsh")]
+impl<T: borsh::ser::BorshSerialize> Encodable<BorshEncoding> for T {
+    fn encode(&self) -> Result<Vec<u8>, borsh::io::Error> {
+        borsh::to_vec(self)
+    }
+}
+
+#[cfg(feature = "borsh")]
+impl<T: borsh::de::BorshDeserialize> Decodable<BorshEncoding> for T {
+    fn decode(bytes: &[u8]) -> Result<Self, borsh::io::Error> {
+        borsh::from_slice(bytes)
+    }
+}
